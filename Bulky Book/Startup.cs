7a8a27c588cb65b72a36f86ef62a5ac1e63b94f2
Vedaiyappan Suruttaiyan <vedaiyappans@stellarit.com>
2020-07-17 17:32:09 +0530

    Code updated

--- Bulky Book/Startup.cs
+++ Bulky Book/Startup.cs
@@ -11,7 +11,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-//using BulkyBook.DataAccess.Data;
 using BulkyBook.DataAccess.Data;
 namespace Bulky_Book
 {
